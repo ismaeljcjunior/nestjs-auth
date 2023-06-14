4f5582ee02acf7849fc805c282d6bb9dc3a311f5
Ismael Junior <ismaeljcjunior@gmail.com>
2023-06-14 10:05:26 -0300

    generate resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
