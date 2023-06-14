06fc31b333fe11b341d9b5ba667a1f8d30e7d88d
Ismael Junior <ismaeljcjunior@gmail.com>
2023-06-14 09:10:32 -0300

    feat prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
